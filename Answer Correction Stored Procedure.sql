fa6c19f0e546e917167c96c6cf2e45c721cadd9b
WafaaElshamandy <88560218+WafaaElshamandy@users.noreply.github.com>
2023-07-21 01:14:26 +0300

    Create Answer Correction Stored Procedure.sql

---
+++ Answer Correction Stored Procedure.sql
@@ -0,0 +1,28 @@
+--Answer Correction--
+alter proc Answer_Correction @ExamId INT, @StudentId INT
+AS
+declare @Grade int
+
+Select @Grade = COUNT(*) 
+From StudAnswer SA JOIN ExamQuest Q ON SA.QID = Q.QID
+JOIN Questions ON Q.QID = Questions.QuestId
+Where Q.EID = @ExamId and SA.SID = @StudentId and SA.Answer = Questions.QestAnsw
+
+select @Grade AS [Your Grade is]
+
+Update Exam
+Set Grade = @Grade
+Where ExamId = @ExamId
+
+UPDATE Exam
+SET Status = 
+  CASE
+    WHEN Grade > 5 THEN 'Passed'
+    ELSE 'Failed'
+  END
+WHERE ExamId = @ExamId;
+
+--Calling--
+declare @ee int
+set @ee =IDENT_CURRENT('Exam')
+execute Answer_Correction @ee,350
